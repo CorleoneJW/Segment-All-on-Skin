0a74b27ac1c6ea192c75bb2bd88f4c93c7d9f63f
Cor4J3 <252653889@qq.com>
2023-08-04 18:30:28 +0800

    writing the draft for the dataset and config

--- datasets/dataset.py
+++ datasets/dataset.py
@@ -2,6 +2,7 @@ import os
 import sys
 import torch
 from torch.utils.data import Dataset
+from torch.utils.data import DataLoader
 from torchvision.transforms import transforms
 import numpy as np
 import collections
@@ -25,43 +26,29 @@ class HAM_datasets(Dataset):
         self.querysz = (
             self.n_way * self.k_query
         )  # number of samples per set for evaluation
-        self.resize = config.resize  # resize to
+        self.resize_h = config.resize_h  # resize height
+        self.resize_w = config.resize_w     # resize height
         self.startidx = config.startidx  # index label not from 0, but from startidx
         self.train = train  # the mode like train and test
+        self.batch_list = []
 
         print(
-            "shuffle DB :%s, b:%d, %d-way, %d-shot, %d-query, "
+            "Train mode :%s, batch_size:%d, %d-way, %d-shot, %d-query, %d-resizeh, %d-resizew"
             % (
                 self.train,
                 self.batch_size,
                 self.n_way,
                 self.k_shot,
                 self.k_query,
-            ),"pixel size(resize)",self.resize
+                self.resize_h,
+                self.resize_w
+            )
         )
 
         if self.train == True:
-            self.transform = transforms.Compose(
-                [
-                    lambda x: Image.open(x).convert("RGB"),
-                    transforms.Resize(self.resize),
-                    # transforms.RandomHorizontalFlip(),
-                    # transforms.RandomRotation(5),
-                    transforms.ToTensor(),
-                    transforms.Normalize(
-                        (0.485, 0.456, 0.406), (0.229, 0.224, 0.225)),
-                ]
-            )
+            self.transform = config.train_transformer
         else:
-            self.transform = transforms.Compose(
-                [
-                    lambda x: Image.open(x).convert("RGB"),
-                    transforms.Resize(self.resize),
-                    transforms.ToTensor(),
-                    transforms.Normalize(
-                        (0.485, 0.456, 0.406), (0.229, 0.224, 0.225)),
-                ]
-            )
+            self.transform = config.test_transformer
 
         """
         path of the dataset (images and masks)
@@ -76,22 +63,77 @@ class HAM_datasets(Dataset):
                 config.test_set, "images")  # path of test set images
             self.mask_path = os.path.join(
                 config.test_set, "masks")  # path of test set mask
-
-        """
-        generating the temp directory for the train or test
-        """
-        # self.create_tempfolder(self.image_path,self.mask_path,self.categories)
-
-    def __getitem__(self, index):
         
-        return 
+        self.class_to_samples = {cls: self.load_samples(cls) for cls in self.categories}
+        
+        # generating the temp directory for the train or test
+        # self.create_tempfolder(self.image_path,self.mask_path,self.categories)
+        
+        # generate the batchlist for __getitem__
+        self.create_batchs()
+
+
+
+    def __getitem__(self, idx):
+        batch_list = self.batch_list
+        return batch_list[idx]
 
     def __len__(self):
         return self.batch_size
 
-    def load_images(self, class_dir):
-        images = [os.path.join(class_dir, img) for img in os.listdir(class_dir)]
-        return images
+    def load_samples(self, cls):
+        images_dir = os.path.join(self.image_path, cls)
+        masks_dir = os.path.join(self.mask_path, cls)
+        image_files = os.listdir(images_dir)
+        samples = [
+            {
+                'image': os.path.join(images_dir, img),
+                'mask': os.path.join(masks_dir, img[:-4]+"_segmentation.png")
+            }
+            for img in image_files
+        ]
+        return samples
+
+    def get_task(self):
+        sampled_classes = random.sample(self.categories, self.n_way)
+        
+        support_set = []
+        query_set = []
+
+        for cls in sampled_classes:
+            sampled_samples = random.sample(self.class_to_samples[cls], self.k_shot + self.k_query)
+            support_samples = sampled_samples[:self.k_shot]
+            query_samples = sampled_samples[self.k_shot:]
+            
+            support_set.extend(support_samples)
+            query_set.extend(query_samples)
+        
+        return support_set, query_set       # [{image:xxx1.jpg, mask:xxx1.png},{image:xxx2.jpg, mask:xxx2.png}]
+
+    def create_batchs(self):
+        batch_size = self.batch_size
+        for time in range(batch_size):
+            support_set, query_set = self.get_task()        # [{image:xxx1.jpg, mask:xxx1.png},{image:xxx2.jpg, mask:xxx2.png}]
+
+            support_images = [Image.open(sample['image']).convert('RGB') for sample in support_set]
+            support_masks = [Image.open(sample['mask']).convert('L') for sample in support_set]
+
+            query_images = [Image.open(sample['image']).convert('RGB') for sample in query_set]
+            query_masks = [Image.open(sample['mask']).convert('L') for sample in query_set]
+
+            if self.transform:
+                support_images = [self.transform(img) for img in support_images]
+                support_masks = [self.transform(mask) for mask in support_masks]
+                
+                query_images = [self.transform(img) for img in query_images]
+                query_masks = [self.transform(mask) for mask in query_masks]
+
+            generation_result = {
+                'support_images': support_images, 'support_masks': support_masks,
+                'query_images': query_images, 'query_masks': query_masks
+            }                           #{'support_images':[images_tensor1,images_tensor2],'support_masks':[masks_tensor1,masks_tensor2]}
+            
+            self.batch_list.append(generation_result)
 
     def create_tempfolder(self, image_path, mask_path, categories):
         # delete the previous folder
@@ -126,7 +168,10 @@ class HAM_datasets(Dataset):
 
 if __name__ == "__main__":
     config = setting_config
-    # ham = HAM_datasets(config, train=True)
-    sampled = random.sample(["1","2","3"],2)
-    print(sampled)
-    
+    dataset = HAM_datasets(config, train=True)
+    train_loader = DataLoader(dataset,batch_size=8)
+    for i,item in enumerate(train_loader):
+        print(i)
+        print(item)
+    # for item in ham:
+    #     print(item)
