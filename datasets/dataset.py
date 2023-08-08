a48186f800485e8efaa4723defe953abbbc81394
Cor4J3 <252653889@qq.com>
2023-08-08 15:46:29 +0800

    change the data that provided by the dataset with tensor [batch_siez,C,W,H]

--- datasets/dataset.py
+++ datasets/dataset.py
@@ -116,18 +116,25 @@ class HAM_datasets(Dataset):
         for time in range(batch_size):
             support_set, query_set = self.get_task()        # [{image:xxx1.jpg, mask:xxx1.png},{image:xxx2.jpg, mask:xxx2.png}]
 
-            support_images = [Image.open(sample['image']).convert('RGB') for sample in support_set]
-            support_masks = [Image.open(sample['mask']).convert('L') for sample in support_set]
+            for i,sample in enumerate(support_set):
+                if i == 0:
+                    support_images = self.transform(Image.open(sample['image']).convert('RGB')).unsqueeze(0)
+                    support_masks = self.transform(Image.open(sample['mask']).convert('L')).unsqueeze(0)
+                else:
+                    support_image = self.transform(Image.open(sample['image']).convert('RGB')).unsqueeze(0)
+                    support_mask = self.transform(Image.open(sample['mask']).convert('L')).unsqueeze(0)
+                    support_images = torch.cat((support_images,support_image),dim=0)
+                    support_masks = torch.cat((support_masks,support_mask),dim=0)
 
-            query_images = [Image.open(sample['image']).convert('RGB') for sample in query_set]
-            query_masks = [Image.open(sample['mask']).convert('L') for sample in query_set]
-
-            if self.transform:
-                support_images = [self.transform(img) for img in support_images]
-                support_masks = [self.mask_transform(mask) for mask in support_masks]
-                
-                query_images = [self.transform(img) for img in query_images]
-                query_masks = [self.mask_transform(mask) for mask in query_masks]
+            for i,sample in enumerate(query_set):
+                if i == 0:
+                    query_images = self.transform(Image.open(sample['image']).convert('RGB')).unsqueeze(0)
+                    query_masks = self.transform(Image.open(sample['mask']).convert('L')).unsqueeze(0)
+                else:
+                    query_image = self.transform(Image.open(sample['image']).convert('RGB')).unsqueeze(0)
+                    query_mask = self.transform(Image.open(sample['mask']).convert('L')).unsqueeze(0)
+                    query_images = torch.cat((query_images,query_image),dim=0)
+                    query_masks = torch.cat((query_masks,query_mask),dim=0)
 
             generation_result = {
                 'support_images': support_images, 'support_masks': support_masks,
